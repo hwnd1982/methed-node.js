585470799584db020b9d93d6fb3641f137e3cf7f
Kirill Lavrov <lkv@myttk.ru>
2023-08-04 17:08:46 +0700

    first lesson completed

---
+++ index.cjs
@@ -0,0 +1,4 @@
+
+const {getExtendedData} = require('extended-data');
+
+console.log('ExtendedData: ', getExtendedData({name: 'kirill  lavrov', dateBirth: '28.10.1982', purpose: 'карьерный рост'}));
---
+++ index.mjs
@@ -0,0 +1,3 @@
+import {getExtendedData} from 'extended-data';
+
+console.log('ExtendedData: ', getExtendedData({name: 'kirill  lavrov', dateBirth: '28.10.1982', purpose: 'карьерный рост'}));
