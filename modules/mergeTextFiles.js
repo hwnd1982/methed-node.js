ae36e8e14ac69a2b1abc11d242d1b6a52ea6ef6f
Kirill Lavrov <lkv@myttk.ru>
2023-08-16 15:39:07 +0700

    fixed redundant nesting

--- modules/mergeTextFiles.js
+++ modules/mergeTextFiles.js
@@ -5,28 +5,26 @@ export const mergeTextFiles = async (dirPath, outputFileName) =>
   (await readdir(dirPath, { withFileTypes: true })).reduce(
     async (promise, file) => {
       if (
-        !file.isDirectory() &&
-        file.name.endsWith('.txt') &&
-        file.name !== outputFileName
+        file.isDirectory() ||
+        !file.name.endsWith('.txt') ||
+        file.name === outputFileName
       ) {
-        const wStream = await promise;
-        try {
-          wStream.write(`[${file.name}]\n`);
-
-          for await (const chunk of createReadStream(
-            `${dirPath}/${file.name}`,
-          )) {
-            wStream.write(chunk);
-          }
-          wStream.write('\n');
-
-          return wStream;
-        } catch (err) {
-          console.log(err.message);
-        }
+        return promise;
       }
 
-      return promise;
+      const wStream = await promise;
+      try {
+        wStream.write(`[${file.name}]\n`);
+
+        for await (const chunk of createReadStream(`${dirPath}/${file.name}`)) {
+          wStream.write(chunk);
+        }
+        wStream.write('\n');
+
+        return wStream;
+      } catch (err) {
+        console.log(err.message);
+      }
     },
     Promise.resolve(createWriteStream(`${dirPath}/${outputFileName}`)),
   );
