90b5a7d8695e3b630fc05ecd5ba7eb0861e8da4a
Kirill Lavrov <lkv@myttk.ru>
2023-09-01 00:18:21 +0700

    url done

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import Questionnaire from './services/Questionnaire.js';
-import {
-  getTextFileNames,
-  replaceTextAll,
-} from './services/TextFileService.js';
+import { parse, format, resolve } from 'node:url';
+import { stringify } from 'node:querystring'; // parse тоже есть;
 
-const questions = [
-  'Введите путь к дирректории: ',
-  'Введите строку для поиска: ',
-  'Введите строку для замены: ',
-];
-
-const app = async () => {
-  const questionnaire = new Questionnaire(questions);
-  const [dirPath, find, replace] = await questionnaire.questioning;
-
-  await replaceTextAll(await getTextFileNames(dirPath), find, replace);
+const myUrl = 'https://methed.ru/path/file.html?user=maks&pass=123#order';
+const parseUrl = parse(myUrl, true); // true - query Object;
+const urlParams = {
+  protocol: 'https:',
+  host: 'methed.ru',
+  hostname: 'methed.ru',
+  hash: '#order',
+  search: '?user=maks&pass=123',
+  pathname: '/path/file.html',
 };
+const formattedUrl = format(urlParams);
+const baseUrl = 'https://methed.ru/';
+const urlPath = '/path/file.html';
+const resolveUrl = resolve(baseUrl, urlPath); // не паримся с лишним слешем;
+const queryParams = stringify(parseUrl.query);
 
-app();
+console.log('parseUrl: ', parseUrl);
+console.log('formattedUrl: ', formattedUrl);
+console.log('resolveUrl: ', resolveUrl);
+console.log('queryParams: ', queryParams);
